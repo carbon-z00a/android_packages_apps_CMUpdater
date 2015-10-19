eb8e6f61a930176f100f73a7f63a528957e04981
Norris, Ryan N <ryan.norris@my.ccsu.edu>
2015-10-19 12:21:56 -0400

    Allow the Updater to run as the system user.
    Although this isn't really required, this is done so the app can get all
    the permissions it wants and/or needs.
    MOAR PERMISSIONS aka GIVE ME MA PERMISSIONS.
    
    Change-Id: I393b15e973392de002e5a29d673c32d2d81a03cc

--- Android.mk
+++ Android.mk
@@ -1,9 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
-
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
@@ -19,9 +17,9 @@ LOCAL_RESOURCE_DIR := \
 LOCAL_AAPT_FLAGS := --auto-add-overlay --extra-packages android.support.v17.leanback
 
 LOCAL_PACKAGE_NAME := CMUpdater
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_PROGUARD_FLAGS := -include $(LOCAL_PATH)/proguard.flags
-
-LOCAL_PRIVILEGED_MODULE := true
 
 include $(BUILD_PACKAGE)
